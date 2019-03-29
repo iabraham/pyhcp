f60c18fb6e9ae8a935410a8507655e35efff4402
Ivan Abraham <itabrah2@illinois.edu>
2019-03-29 16:43:35 -0500

    Speed up download by generator expressions

--- automate.py
+++ automate.py
@@ -1,9 +1,9 @@
 from download_hcp import do_subject
 import multiprocessing as mp 
-import gzip, pickle
 from rpy2.robjects.packages import importr
 import zipshelve
 from pickle import HIGHEST_PROTOCOL
+from datetime import datetime
 
 
 def main():
@@ -18,32 +18,37 @@ def main():
         utils = importr('utils')
         utils.install_packages('cifti')
 
-
     with open('subjectlist.txt') as stream:
         subject_ids = stream.readlines()
 
-    
     # Strip newline characters
     subject_ids = [idx.strip() for idx in subject_ids]
+    fin = 'HCP_1200/hcp_data'
 
     # Download and process. `procs` is # of processors
     procs = 4
-    
+    print(datetime.now())
     with mp.Pool(procs) as pool:
         result = zip(subject_ids, pool.map(do_subject, subject_ids))
-    
+
     print('Shelving returned data')
-    fin = 'HCP_1200/hcp_data'
 
     with zipshelve.open(fin, protocol=HIGHEST_PROTOCOL) as shelf:
         for key, value in result:
             shelf[key] = value
 
+    print(datetime.now())
+
     # Serial instead of parallel?
 
-#    for idx in subject_ids:
-#        datum = do_subject(idx)
-#        breakpoint()
+    # datum = dict()
+    # for idx in subject_ids:
+    #     datum[idx] = do_subject(idx)
+    #
+    # with zipshelve.open(fin, protocol=HIGHEST_PROTOCOL) as shelf:
+    #     for key, value in datum:
+    #         shelf[key] = value
+
 
 
 if __name__ == "__main__":
